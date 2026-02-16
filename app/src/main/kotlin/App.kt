dfae10cdf335d1a0585f1b71f64694af88befdaa
Hien V.Mai <hienmv.vn@gmail.com>
2026-02-16 22:41:20 +0900

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -106,7 +106,7 @@ data class HttpRequestLine(
 
 enum class HttpStatus(val code: Int, val message: String) {
     OK_200(code = 200, message = "OK"),
-    NOTFOUND_404(code = 404, message = "Not found")
+    NOTFOUND_404(code = 404, message = "Not Found")
 }
 
 enum class HttpContentType(val value: String) {
