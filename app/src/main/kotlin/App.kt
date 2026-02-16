b79f2e6b7456551300a9d34937739edfb5fd86d0
Hien V.Mai <hienmv.vn@gmail.com>
2026-02-16 22:39:50 +0900

    handle echo request

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -16,15 +16,123 @@ fun main() {
             val input = Scanner(client.getInputStream())
             val outputStream = client.getOutputStream()
 
-            val startLine = input.nextLine()
+            val httpRequestLine = HttpRequestLine.parse(input.nextLine())
             val response =
-                if (startLine == "GET / HTTP/1.1") {
-                    "HTTP/1.1 200 OK\r\n\r\n"
+                if (httpRequestLine?.path?.value == "/") {
+                    HttpResponseMessage(status = HttpStatus.OK_200)
+                } else if (httpRequestLine?.path?.value?.startsWith("/echo/") == true) {
+                    val body = httpRequestLine.path.value.removePrefix("/echo/")
+                    HttpResponseMessage(
+                        status = HttpStatus.OK_200,
+                        representationHeader = HttpRepresentationHeader(
+                            contentType = HttpContentType.TEXT,
+                            contentLength = body.toByteArray().size.toLong()
+                        ),
+                        body = body
+                    )
                 } else {
-                    "HTTP/1.1 404 Not Found\r\n\r\n"
+                    HttpResponseMessage(status = HttpStatus.NOTFOUND_404)
                 }
-            outputStream.write(response.toByteArray())
+            outputStream.write(response.toString().toByteArray())
             outputStream.flush()
         }
+    }
+}
+
+enum class HttpMethod {
+    GET, POST, PUT, DELETE
+}
+
+enum class HttpProtocol(val value: String) {
+    HTTP1("HTTP/1.0"),
+    HTTP11("HTTP/1.1");
+
+
+    companion object {
+        fun fromValue(value: String): HttpProtocol = entries.first { it.value == value }
+    }
+}
+
+enum class HttpRequestTargetType {
+    ORIGINAL_FORM,
+    ABSOLUTE_FORM,
+    AUTHORITY_FORM,
+    ASTERISK_FORM
+}
+
+data class HttpRequestTarget(
+    val type: HttpRequestTargetType,
+    val value: String
+) {
+    init {
+        // currently, accept only original form request target
+        if (type == HttpRequestTargetType.ORIGINAL_FORM) {
+            require(value.startsWith("/")) {
+                throw Exception("invalid original form target")
+            }
+        } else {
+            throw Exception("not support request target")
+        }
+    }
+}
+
+data class HttpRequestLine(
+    val method: HttpMethod,
+    val path: HttpRequestTarget,
+    val protocol: HttpProtocol,
+) {
+    companion object {
+        fun parse(line: String): HttpRequestLine? {
+            val parts = line.split(" ")
+            if (parts.size != 3) {
+                return null
+            }
+            val (method, path, protocol) = parts
+            return try {
+                HttpRequestLine(
+                    method = HttpMethod.valueOf(method),
+                    path = HttpRequestTarget(
+                        type = HttpRequestTargetType.ORIGINAL_FORM,
+                        value = path
+                    ),
+                    protocol = HttpProtocol.fromValue(protocol)
+                )
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
+}
+
+enum class HttpStatus(val code: Int, val message: String) {
+    OK_200(code = 200, message = "OK"),
+    NOTFOUND_404(code = 404, message = "Not found")
+}
+
+enum class HttpContentType(val value: String) {
+    JSON("application/json"),
+    TEXT("text/plain"),
+    HTML("text/html")
+}
+
+data class HttpRepresentationHeader(
+    val contentType: HttpContentType,
+    val contentLength: Long,
+)
+
+data class HttpResponseMessage(
+    val status: HttpStatus,
+    val protocol: HttpProtocol = HttpProtocol.HTTP11,
+    val representationHeader: HttpRepresentationHeader? = null,
+    val body: String? = null
+) {
+    override fun toString(): String {
+        val crlf = "\r\n"
+        val statusLine = listOf(protocol.value, status.code, status.message).joinToString(" ")
+        val representationHeaderLines = representationHeader?.let {
+            "Content-Type: ${it.contentType.value}${crlf}Content-Length: ${it.contentLength}"
+        } ?: ""
+        // Status + CRLF + Headers + CRLF + CRLF + Body
+        return "$statusLine$crlf$representationHeaderLines$crlf$crlf${body ?: ""}"
     }
 }
