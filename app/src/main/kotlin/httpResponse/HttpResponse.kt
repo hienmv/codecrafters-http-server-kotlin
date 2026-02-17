320c3d100cb079d82348a67de050b411cfa294fd
Hien V.Mai <hienmv.vn@gmail.com>
2026-02-17 14:34:53 +0900

    handle accept-encoding request

--- app/src/main/kotlin/httpResponse/HttpResponse.kt
+++ app/src/main/kotlin/httpResponse/HttpResponse.kt
@@ -8,15 +8,18 @@ data class HttpResponse(
     val status: HttpStatus,
     val protocol: HttpProtocol = HttpProtocol.HTTP11,
     val contentType: HttpContentType? = null,
-    val content: String? = null
+    val contentEncoding: String? = null,
+    val body: String? = null
 ) {
     override fun toString(): String {
         val statusLine = listOf(protocol.value, status.code, status.message).joinToString(" ")
-        val body = content ?: ""
         val headerLines = contentType?.let {
-            "Content-Type: ${contentType.value}${Constant.CRLF}Content-Length: ${body.toByteArray().size}"
+            val contentTypeLine = "Content-Type: ${contentType.value}"
+            val contentEncodingLine = contentEncoding?.let { "Content-Encoding: $it" } ?: ""
+            val contentLengthLine = body?.let { "Content-Length: ${it.toByteArray().size}" } ?: ""
+            "$contentTypeLine${Constant.CRLF}$contentEncodingLine${Constant.CRLF}$contentLengthLine"
         } ?: ""
         // Status + CRLF + Headers + CRLF + CRLF + Body
-        return "$statusLine${Constant.CRLF}$headerLines${Constant.CRLF}${Constant.CRLF}$body"
+        return "$statusLine${Constant.CRLF}$headerLines${Constant.CRLF}${Constant.CRLF}${body ?: ""}"
     }
 }
