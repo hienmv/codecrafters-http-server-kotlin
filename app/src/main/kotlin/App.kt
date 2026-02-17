5414c3d902eadc07e31a83b1a7588f8a6f7bfd4f
Hien V.Mai <hienmv.vn@gmail.com>
2026-02-17 12:20:25 +0900

    handle concurrent connections with platform threads

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -3,6 +3,7 @@ import httpRequest.HttpRequest
 import httpResponse.HttpResponse
 import httpResponse.HttpStatus
 import java.net.ServerSocket
+import java.net.Socket
 
 fun main() {
     val serverSocket = ServerSocket(4221)
@@ -12,28 +13,33 @@ fun main() {
     serverSocket.reuseAddress = true
 
     while (true) { // keep server running
-        serverSocket.accept().use { client ->
-            val outputStream = client.getOutputStream()
-            val request = HttpRequest.parse(client.getInputStream())
-            val response = if (request?.requestTarget == "/") {
-                HttpResponse(status = HttpStatus.OK_200)
-            } else if (request?.requestTarget?.startsWith("/echo/") == true) {
-                HttpResponse(
-                    status = HttpStatus.OK_200,
-                    contentType = HttpContentType.TEXT,
-                    content = request.requestTarget.removePrefix("/echo/")
-                )
-            } else if (request?.requestTarget == "/user-agent") {
-                HttpResponse(
-                    status = HttpStatus.OK_200,
-                    contentType = HttpContentType.TEXT,
-                    content = request.requestHeaders["User-Agent"]
-                )
-            } else {
-                HttpResponse(status = HttpStatus.NOTFOUND_404)
-            }
-            outputStream.write(response.toString().toByteArray())
-            outputStream.flush()
-        }
+        val client = serverSocket.accept()
+        Thread { // initialize a platform thread
+            client.use { handle(it) }
+        }.start()
     }
+}
+
+fun handle(client: Socket) {
+    val outputStream = client.getOutputStream()
+    val request = HttpRequest.parse(client.getInputStream())
+    val response = if (request?.requestTarget == "/") {
+        HttpResponse(status = HttpStatus.OK_200)
+    } else if (request?.requestTarget?.startsWith("/echo/") == true) {
+        HttpResponse(
+            status = HttpStatus.OK_200,
+            contentType = HttpContentType.TEXT,
+            content = request.requestTarget.removePrefix("/echo/")
+        )
+    } else if (request?.requestTarget == "/user-agent") {
+        HttpResponse(
+            status = HttpStatus.OK_200,
+            contentType = HttpContentType.TEXT,
+            content = request.requestHeaders["User-Agent"]
+        )
+    } else {
+        HttpResponse(status = HttpStatus.NOTFOUND_404)
+    }
+    outputStream.write(response.toString().toByteArray())
+    outputStream.flush()
 }
